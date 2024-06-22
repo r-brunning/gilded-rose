03188da5cd5be26780a071f0721bc66dad3cae9a
RBrunning <you@example.com>
2024-06-22 17:22:44 +0100

    Add test for updating sellIn and quality once sell by date is passed

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -8,4 +8,12 @@ describe('Gilded Rose', () => {
     expect(items[0].sellIn).toBe(9);
     expect(items[0].quality).toBe(19);
   });
+
+  it('should decrease sellIn and quality for normal items by two once sell by date is passed', () => {
+    const gildedRose = new GildedRose([new Item('normal item', 0, 20)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('normal item');
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(18);
+  });
 });

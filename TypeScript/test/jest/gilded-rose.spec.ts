2717b0132a24cee804476c557d4bf44a53766a09
RBrunning <you@example.com>
2024-06-22 17:17:15 +0100

    Update test name for normal items to be more descriptive

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,11 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+  it('should decrease sellIn and quality for normal items by one', () => {
+    const gildedRose = new GildedRose([new Item('normal item', 10, 20)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('foo');
+    expect(items[0].name).toBe('normal item');
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(19);
   });
 });

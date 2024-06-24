e8dc7a772d0a7a3a334894ac88df6275fc362178
RBrunning <you@example.com>
2024-06-24 21:05:43 +0100

    refactor: introduced helper methods for quality range checks and simplified conditionals

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -10,6 +10,9 @@ export class Item {
   }
 }
 
+const MIN_QUALITY = 0;
+const MAX_QUALITY = 50;
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -19,50 +22,39 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
-      if (item.name === 'Sulfuras, Hand of Ragnaros') {
-        continue;
-      }
+      if (item.name === 'Sulfuras, Hand of Ragnaros') continue;
 
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          item.quality = item.quality - 1
+      if (item.name === 'Aged Brie' || item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        this.canIncreaseValue(item) && item.quality++;
+
+        if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          item.sellIn < 11 && this.canIncreaseValue(item) && item.quality++;
+          item.sellIn < 6 && this.canIncreaseValue(item) && item.quality++;
         }
       } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
-        }
+        this.canDecreaseValue(item) && item.quality--;
       }
-      item.sellIn = item.sellIn - 1;
+
+      item.sellIn -= 1;
 
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              item.quality = item.quality - 1
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
+        if (item.name === 'Aged Brie') {
+          this.canIncreaseValue(item) && item.quality++;
+        } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          item.quality = 0;
+        } else if (this.canDecreaseValue(item)) {
+          item.quality--;
         }
       }
     }
-
     return this.items;
+  }
+
+  private canIncreaseValue(item: Item) {
+    return item.quality < MAX_QUALITY;
+  }
+
+  private canDecreaseValue(item: Item) {
+    return item.quality > MIN_QUALITY;
   }
 }

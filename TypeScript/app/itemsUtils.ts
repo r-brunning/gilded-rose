9c70df7de29aba2078afe58e5dbddf095a77ce25
RBrunning <you@example.com>
2024-06-25 10:52:09 +0100

    refactor: reintroduce if statements to make total quality changes more understandable and use Math.min/max to keep values within range

--- TypeScript/app/itemsUtils.ts
+++ TypeScript/app/itemsUtils.ts
@@ -4,31 +4,45 @@ export const MIN_QUALITY = 0;
 export const MAX_QUALITY = 50;
 
 export function handleAgedBrie(item: Item) {
-  if (canIncreaseValue(item)) item.quality++;
-  if (item.sellIn <= 0 && canIncreaseValue(item)) item.quality++;
+  if (item.sellIn > 0) {
+    increaseQuality(item);
+  } else {
+    increaseQuality(item, 2);
+  }
 }
 
 export function handleBackstagePasses(item: Item) {
-  if (canIncreaseValue(item)) item.quality++;
-  if (item.sellIn <= 10 && canIncreaseValue(item)) item.quality++;
-  if (item.sellIn <= 5 && canIncreaseValue(item)) item.quality++;
-  if (item.sellIn <= 0) item.quality = 0;
+  if (item.sellIn > 10) {
+    increaseQuality(item);
+  } else if (item.sellIn > 5) {
+    increaseQuality(item, 2);
+  } else if (item.sellIn > 0) {
+    increaseQuality(item, 3);
+  } else {
+    item.quality = 0;
+  }
 }
 
 export function handleNormalItems(item: Item) {
-  if (canDecreaseValue(item)) item.quality--;
-  if (item.sellIn <= 0 && canDecreaseValue(item)) item.quality--;
+  if (item.sellIn > 0) {
+    decreaseQuality(item);
+  } else {
+    decreaseQuality(item, 2);
+  }
 }
 
 export function handleConjuredItems(item: Item) {
-  if (canDecreaseValue(item)) item.quality-=2;
-  if (item.sellIn <= 0 && canDecreaseValue(item)) item.quality-=2;
+  if (item.sellIn > 0) {
+    decreaseQuality(item, 2);
+  } else {
+    decreaseQuality(item, 4);
+  }
 }
 
-export function canIncreaseValue(item: Item) {
-  return item.quality < MAX_QUALITY;
+export function increaseQuality(item: Item, amount: number = 1) {
+  item.quality = Math.min(item.quality + amount, MAX_QUALITY);
 }
 
-export function canDecreaseValue(item: Item) {
-  return item.quality > MIN_QUALITY;
+export function decreaseQuality(item: Item, amount: number = 1) {
+  item.quality = Math.max(item.quality - amount, MIN_QUALITY);
 }
